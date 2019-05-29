3ec80b1e6b8ab2cf500e9fd62d23a30642e77661
Haixiang <haixiang6123@gmail.com>
2019-05-28 20:15:21 -0700

    Update auth module

--- src/main/java/hello/service/UserService.java
+++ src/main/java/hello/service/UserService.java
@@ -1,19 +1,49 @@
 package hello.service;
 
-import hello.entity.User;
-import hello.mapper.UserMapper;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
-public class UserService {
-    private UserMapper userMapper;
+@Service
+public class UserService implements UserDetailsService {
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private Map<String, String> userPasswords = new ConcurrentHashMap<>();
 
-    @Inject
-    public UserService(UserMapper userMapper) {
-        this.userMapper = userMapper;
+    public UserService(Map<String, String> userPasswords) {
+        this.userPasswords = userPasswords;
     }
 
-    public User getUserById(Integer id) {
-        return userMapper.findUserById(id);
+    @Inject
+    public UserService(BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        register("admin", "123");
+    }
+
+    public void register(String username, String password) {
+        userPasswords.put(username, bCryptPasswordEncoder.encode(password));
+    }
+
+    public String getPassword(String username) {
+        return userPasswords.get(username);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // Check if user exist
+        if (!userPasswords.containsKey(username)) {
+            throw new UsernameNotFoundException(username + " not found");
+        }
+
+        // Check if password is correct
+        String encodedPassword = userPasswords.get(username);
+
+        return new org.springframework.security.core.userdetails.User(username, encodedPassword, Collections.emptyList());
     }
 }
